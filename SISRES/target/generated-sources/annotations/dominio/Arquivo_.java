9346f9fdfb0ebc048e7bdce5e41db6ec58cca942
Natália Amâncio <nataliaamancio19@gmail.com>
2018-01-02 22:51:43 -0300

    Ajustes
    Ajustes

--- SISRES/target/generated-sources/annotations/dominio/Arquivo_.java
+++ SISRES/target/generated-sources/annotations/dominio/Arquivo_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-01-02T00:14:31")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-01-02T21:01:50")
 @StaticMetamodel(Arquivo.class)
 public class Arquivo_ extends EntidadeNegocio_ {
 
--- SISRES/target/generated-sources/annotations/dominio/Requerimento_.java
+++ SISRES/target/generated-sources/annotations/dominio/Requerimento_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-01-02T00:14:31")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-01-02T21:01:50")
 @StaticMetamodel(Requerimento.class)
 public class Requerimento_ extends EntidadeNegocio_ {
 
